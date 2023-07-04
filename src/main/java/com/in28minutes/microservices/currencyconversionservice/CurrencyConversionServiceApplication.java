a94be74c2faa432a22ad29bbcbc64523c91b99eb
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-07-04 09:30:06 -0400

    Bring in openfeign. Having a tough time getting it recognized though.

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,4 @@
+package com.in28minutes.microservices.currencyconversionservice;
+
+public interface CurrencyExchangeProxy {
+}
